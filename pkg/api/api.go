f57b79a622ce178a81803319538a32932258c52e
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-10-26 14:14:25 -0300

    Add fields to GetIdentity request and response

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -11,9 +11,34 @@ type CustomersV1 interface {
 	CreateCustomer(ctx context.Context, req CreateCustomerRequest) (CreateCustomerResponse, error)
 }
 
-type GetCustomerRequest struct{}
+// GetIdentityRequest is used to get the identity of a certain customer or user.
+// If User is passed, it returns the Customer.
+// If Customer is passed, it returns the User.
+// If both values are passed, it returns an error.
+type GetIdentityRequest struct {
+	// User is the username of a certain application that will be returned.
+	// Mutually exclusive with Customer.
+	User string
+	// Customer is the customer id of the user that will be returned.
+	// Mutually exclusive with User.
+	Customer string
+	// Service is the payment service used to register the customer.
+	Service string
+	// Application is the application that originated the creation of the customer.
+	Application string
+}
 
-type GetCustomerResponse struct{}
+// GetIdentityResponse is used to return a user or a customer depending on the parameters set in GetIdentityRequest.
+type GetIdentityResponse struct {
+	// User contains the username.
+	User *string
+	// Customer contains the customer id.
+	Customer *string
+	// Service is the payment service used to register the customer.
+	Service string
+	// Application is the application that originated the creation of the customer.
+	Application string
+}
 
 type CreateCustomerRequest struct{}
 

1cf9c0f971eb94a892262d43cf97a683ead4e5ea
Marcos Huck <marcos.h3012@gmail.com>
2021-10-27 17:45:22 +0000

    Project structure

---
+++ pkg/client/client.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"context"
+	"gitlab.com/ignitionrobotics/billing/customers/pkg/api"
+)
+
+// client contains the HTTP client to connect to a api.CustomersV1 service.
+type client struct{}
+
+// GetIdentity performs an HTTP request to get a customer or a user of a certain application.
+func (c *client) GetIdentity(ctx context.Context, req api.GetIdentityRequest) (api.GetIdentityResponse, error) {
+	panic("implement me")
+}
+
+// CreateCustomer performs an HTTP request to create a new customer for a certain application.
+func (c *client) CreateCustomer(ctx context.Context, req api.CreateCustomerRequest) (api.CreateCustomerResponse, error) {
+	panic("implement me")
+}
+
+// Client holds methods to interact with the api.CustomersV1.
+type Client interface {
+	api.CustomersV1
+}
+
+// NewClient initializes a new api.CustomersV1 client implementation using an HTTP client.
+func NewClient() Client {
+	return &client{}
+}

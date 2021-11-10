d23959d40eea8eb69c142d9c27c93f6d6ea9423a
Marcos Huck <marcos.h3012@gmail.com>
2021-11-10 14:43:38 +0000

    Implement HTTP client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,24 +3,43 @@ package client
 import (
 	"context"
 	"gitlab.com/ignitionrobotics/billing/customers/pkg/api"
+	"gitlab.com/ignitionrobotics/web/ign-go/encoders"
+	"gitlab.com/ignitionrobotics/web/ign-go/net"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 // client contains the HTTP client to connect to a api.CustomersV1 service.
-type client struct{}
+type client struct {
+	client net.Client
+}
 
 // GetCustomerByHandle performs an HTTP request to get a customer based on its handle.
-func (c *client) GetCustomerByHandle(ctx context.Context, req api.GetCustomerByHandleRequest) (api.CustomerResponse, error) {
-	panic("implement me")
+func (c *client) GetCustomerByHandle(ctx context.Context, in api.GetCustomerByHandleRequest) (api.CustomerResponse, error) {
+	var out api.CustomerResponse
+	if err := c.client.Call(ctx, "GetCustomerByHandle", &in, &out); err != nil {
+		return api.CustomerResponse{}, err
+	}
+	return out, nil
 }
 
 // GetCustomerByID performs an HTTP request to get a customer based on its id.
-func (c *client) GetCustomerByID(ctx context.Context, req api.GetCustomerByIDRequest) (api.CustomerResponse, error) {
-	panic("implement me")
+func (c *client) GetCustomerByID(ctx context.Context, in api.GetCustomerByIDRequest) (api.CustomerResponse, error) {
+	var out api.CustomerResponse
+	if err := c.client.Call(ctx, "GetCustomerByID", &in, &out); err != nil {
+		return api.CustomerResponse{}, err
+	}
+	return out, nil
 }
 
 // CreateCustomer performs an HTTP request to create a new customer for a certain application.
-func (c *client) CreateCustomer(ctx context.Context, req api.CreateCustomerRequest) (api.CustomerResponse, error) {
-	panic("implement me")
+func (c *client) CreateCustomer(ctx context.Context, in api.CreateCustomerRequest) (api.CustomerResponse, error) {
+	var out api.CustomerResponse
+	if err := c.client.Call(ctx, "CreateCustomer", &in, &out); err != nil {
+		return api.CustomerResponse{}, err
+	}
+	return out, nil
 }
 
 // Client holds methods to interact with the api.CustomersV1.
@@ -28,7 +47,23 @@ type Client interface {
 	api.CustomersV1
 }
 
-// NewClient initializes a new api.CustomersV1 client implementation using an HTTP client.
-func NewClient() Client {
-	return &client{}
+// NewCustomersClientV1 initializes a new api.CustomersV1 client implementation using an HTTP client.
+func NewCustomersClientV1(baseURL *url.URL, timeout time.Duration) Client {
+	endpoints := map[string]net.EndpointHTTP{
+		"GetCustomerByHandle": {
+			Method: http.MethodPost,
+			Path:   "/customers/search/handle",
+		},
+		"GetCustomerByID": {
+			Method: http.MethodPost,
+			Path:   "/customers/search/id",
+		},
+		"CreateCustomer": {
+			Method: http.MethodPost,
+			Path:   "/customers",
+		},
+	}
+	return &client{
+		client: net.NewClient(net.NewCallerHTTP(baseURL, endpoints, timeout), encoders.JSON),
+	}
 }

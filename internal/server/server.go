723dcec1ea5e154da732b6f455cd9476f0a3c9ea
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-11-12 17:34:14 -0300

    Remove content type whitelist

--- internal/server/server.go
+++ internal/server/server.go
@@ -97,7 +97,6 @@ func NewServer(opts Options) *Server {
 	s.router.Use(middleware.RealIP)
 	s.router.Use(middleware.Logger)
 	s.router.Use(middleware.Recoverer)
-	s.router.Use(middleware.AllowContentType("application/json"))
 	s.router.Use(render.SetContentType(render.ContentTypeJSON))
 
 	s.router.Route("/customers", func(r chi.Router) {
